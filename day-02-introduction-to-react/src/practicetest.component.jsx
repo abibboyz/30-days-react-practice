05a3742b56d57af5f2131a30b8d92693fab7d5d5
AbiOnMac <abi.kh03@gmail.com>
2024-01-13 12:15:17 +1100

    Intorduction to react and jsx elements in it.

--- day-02-introduction-to-react/src/practicetest.component.jsx
+++ day-02-introduction-to-react/src/practicetest.component.jsx
@@ -1,5 +1,27 @@
 export const PracticeText = () => {
+
+  const welcome = 'Welcome to 30 Days Of React'
+  const title = 'Getting Started React'
+  const subtitle = 'JavaScript Library'
+  const author = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+  }
+  const date = 'Oct 2, 2020'
+  const copyRight = 'Copyright 2020'
+  const footer = (
+    <footer><div className='footer-wrapper'><p>{copyRight}</p></div></footer>
+  )
+
+
   return (
-    <h1>30 Days Of practice Started</h1>
+    <div>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <h4>{`${author.firstName} ${author.lastName}`}</h4>
+      <h5>{date}</h5>
+      <h6>{footer}</h6>
+    </div>
   )
 };
